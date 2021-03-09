f2b05fe242a427829c28830c93e52522bfcad6b3
ShahRukh <shahaider@gmail.com>
2021-03-09 10:32:43 +0500

    create project

---
+++ Code/Paypixl/Paypixl/PaypixlApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaypixlApp.swift
+//  Paypixl
+//
+//  Created by Syed ShahRukh Haider on 09/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct PaypixlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
