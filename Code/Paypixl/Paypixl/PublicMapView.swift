b7ef235493827f955e8ef10151caccacebae41bf
ShahRukh <perception.khi@gmail.com>
2021-03-12 17:10:43 +0500

    publicmapview file created

---
+++ Code/Paypixl/Paypixl/PublicMapView.swift
@@ -0,0 +1,20 @@
+//
+//  PublicMapView.swift
+//  Paypixl
+//
+//  Created by Syed ShahRukh Haider on 12/03/2021.
+//
+
+import SwiftUI
+
+struct PublicMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PublicMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        PublicMapView()
+    }
+}
